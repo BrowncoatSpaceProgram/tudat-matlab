0a3138aa675d7e11700ebecdcb79e8edfa6924cb
Aleix Pinardell <J.A.PinardellPons@student.tudelft.nl>
2017-08-30 15:17:42 +0200

    Catch number of failures bug fix

--- src/+test/runUnitTest.m
+++ src/+test/runUnitTest.m
@@ -2,7 +2,7 @@ function failcount = runUnitTest(testName)
 
 failcount = 0;
 [status,output] = system(fullfile(tudat.testsBinariesDirectory,['test_json_' testName]),'-echo');
-[~,tok] = regexp(output,'\*\*\* (\d+) failures','match','tokens');
+[~,tok] = regexp(output,'\*\*\* (\d+) failure','match','tokens');
 if ~isempty(tok)
     failcount = str2double(tok{1}{1});
 elseif status
