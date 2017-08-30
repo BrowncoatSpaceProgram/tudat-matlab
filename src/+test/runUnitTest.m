9ce447cb5908ace746d5460152a38c21cd1590de
Aleix Pinardell <J.A.PinardellPons@student.tudelft.nl>
2017-08-30 14:23:42 +0200

    Throw matlab error if C++ error when running tests

--- src/+test/runUnitTest.m
+++ src/+test/runUnitTest.m
@@ -1,8 +1,10 @@
 function failcount = runUnitTest(testName)
 
 failcount = 0;
-[~,output] = system(fullfile(tudat.testsBinariesDirectory,['test_json_' testName]),'-echo');
-[~,tok] = regexp(output,'\*\*\* (\d+)','match','tokens');
+[status,output] = system(fullfile(tudat.testsBinariesDirectory,['test_json_' testName]),'-echo');
+[~,tok] = regexp(output,'\*\*\* (\d+) failures','match','tokens');
 if ~isempty(tok)
     failcount = str2double(tok{1}{1});
+elseif status
+    error('Terminated with status: %g',status);
 end
