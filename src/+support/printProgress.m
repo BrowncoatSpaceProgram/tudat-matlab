c47dce9560d74d041ebee0649bfa13ca677ecd6b
Aleix Pinardell <J.A.PinardellPons@student.tudelft.nl>
2017-09-03 13:20:02 +0200

    Enhanced support.printProgress function

--- src/+support/printProgress.m
+++ src/+support/printProgress.m
@@ -1,11 +1,13 @@
-function current = printProgress(current,previous,bar,precision)
+function current = printProgress(current,previous,timer,bar,precision)
 
-if nargin < 4
+if nargin < 5
     precision = 0;
-end
-
-if nargin < 3
-    bar = 0;
+    if nargin < 4
+        bar = true;
+        if nargin < 3
+            timer = [];
+        end
+    end
 end
 
 currentStr = sprintf(sprintf('%%.%if%%%%',precision),current*100);
@@ -18,9 +20,23 @@ else
 end
 
 if updateProgress
+    if ~isempty(timer) && current > 0
+        timeRemaining = toc(timer)/current*(1 - current);
+        units = {'s','min','h','d'};
+        times = zeros(size(units));
+        for i = 1:length(units)
+            times(i) = convert.fromSI(timeRemaining,units{i});
+        end
+        i = find(times>1,1,'last');
+        if isempty(i)
+            i = 1;
+        end
+        currentStr = [currentStr sprintf(' (%i %s remaining)',round(times(i)),units{i})];
+    end
+    
     output = '';
     if bar
-        full = round(current*100);
+        full = floor(current*100);
         for i = 1:full
             output = sprintf('%s\x25FC',output);
         end
