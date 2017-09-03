848987871647b35d7d30590aca858cb0eefeed14
Aleix Pinardell <J.A.PinardellPons@student.tudelft.nl>
2017-09-03 15:24:40 +0200

    Match Earth's gm to Spice value

--- src/+constants/standardGravitationalParameter.m
+++ src/+constants/standardGravitationalParameter.m
@@ -1,6 +1,6 @@
 classdef standardGravitationalParameter
    properties (Constant)
-      earth = 3.9860044189e14
+      earth = 3.986004418e14
       moon  = 4.90486959e12
       sun   = 1.327124400189e20
    end
