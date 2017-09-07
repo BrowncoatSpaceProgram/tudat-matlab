c7b6bf0dc1c46cfb0e39d6dfaf8e42eeee073871
Aleix Pinardell <J.A.PinardellPons@student.tudelft.nl>
2017-09-07 17:01:01 +0200

    Add unit test simulationThrustAccelerationFromFile

---
+++ tests/simulationThrustAccelerationFromFile.m
@@ -0,0 +1,59 @@
+function failcount = simulationThrustAccelerationFromFile
+
+tudat.load();
+
+% Simulation
+simulation = Simulation(0,convert.toSI(14,'d'),'SSB','J2000');
+simulation.spice = Spice('pck00009.tpc','de-403-masses.tpc','de421.bsp');
+simulation.spice.preloadKernels = false;
+
+% Bodies
+vehicle = Body('vehicle');
+vehicle.initialState.x = 8e6;
+vehicle.initialState.vy = 7500;
+vehicle.mass = 5000;
+simulation.addBodies(Earth,vehicle);
+simulation.bodies.Earth.ephemeris = ConstantEphemeris(zeros(1,6));
+simulation.bodies.Earth.ephemeris.frameOrientation = 'J2000';
+simulation.bodies.Earth.gravityField.type = GravityFields.pointMassSpice;
+
+% Gravitational accelerations
+accelerationsOnVehicle.Earth = PointMassGravity();
+
+% Thrust acceleration
+thrust = Thrust();
+thrust.dataInterpolation.data = FromFileDataMap('thrustValues.txt');
+thrust.dataInterpolation.interpolator.type = Interpolators.linear;
+thrust.specificImpulse = 3000;
+thrust.frame = ThrustFrames.lvlh;
+thrust.centralBody = Earth;
+accelerationsOnVehicle.vehicle = thrust;
+
+% Translational propagator
+translationalPropagator = TranslationalPropagator();
+translationalPropagator.centralBodies = Earth;
+translationalPropagator.bodiesToPropagate = vehicle;
+translationalPropagator.accelerations.vehicle = accelerationsOnVehicle;
+
+% Mass propagator
+massPropagator = MassPropagator();
+massPropagator.bodiesToPropagate = vehicle;
+massPropagator.massRateModels.vehicle = FromThrustMassRateModel();
+
+% Hybrid propagator
+simulation.propagators = {translationalPropagator, massPropagator};
+
+% Integrator
+simulation.integrator.type = Integrators.rungeKutta4;
+simulation.integrator.stepSize = 30;
+
+% Dependent variables
+simulation.addResultsToSave('thrust','vehicle.acceleration@thrust-vehicle');
+simulation.addResultsToSave('rotation','vehicle.lvlhToInertialFrameRotation-Earth');
+
+% Generate input file
+test.createInput(simulation,fullfile(mfilename,'main'));
+
+% Run test
+failcount = test.runUnitTest(mfilename);
+
