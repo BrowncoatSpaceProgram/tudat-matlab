601ea3df062c3613115d3e2f907717352f63756b
Aleix Pinardell <J.A.PinardellPons@student.tudelft.nl>
2017-09-07 16:20:30 +0200

    Unit test simulationThrustAlongVelocityVector

---
+++ tests/simulationThrustAlongVelocityVector.m
@@ -0,0 +1,56 @@
+function failcount = simulationThrustAlongVelocityVector
+
+tudat.load();
+
+% Simulation
+simulation = Simulation(0,convert.toSI(14,'d'));
+simulation.spice = Spice('pck00009.tpc','de-403-masses.tpc','de421.bsp');
+simulation.spice.preloadKernels = false;
+
+% Bodies
+vehicle = Body('vehicle');
+vehicle.initialState.x = 8e6;
+vehicle.initialState.vy = 7500;
+vehicle.mass = 5000;
+simulation.addBodies(Sun,Earth,Moon,vehicle);
+
+% Gravitational accelerations
+accelerationsOnVehicle.Earth = PointMassGravity();
+accelerationsOnVehicle.Sun = PointMassGravity();
+accelerationsOnVehicle.Moon = PointMassGravity();
+
+% Thrust acceleration
+thrust = Thrust();
+thrust.direction.type = ThrustDirections.colinearWithStateSegment;
+thrust.direction.relativeBody = Earth;
+thrust.direction.colinearWithVelocity = true;
+thrust.direction.towardsRelativeBody = false;
+thrust.magnitude = ConstantThrustMagnitude();
+thrust.magnitude.constantMagnitude = 25;
+thrust.magnitude.specificImpulse = 5000;
+accelerationsOnVehicle.vehicle = thrust;
+
+% Translational propagator
+translationalPropagator = TranslationalPropagator();
+translationalPropagator.centralBodies = Earth;
+translationalPropagator.bodiesToPropagate = vehicle;
+translationalPropagator.accelerations.vehicle = accelerationsOnVehicle;
+
+% Mass propagator
+massPropagator = MassPropagator();
+massPropagator.bodiesToPropagate = vehicle;
+massPropagator.massRateModels.vehicle = FromThrustMassRateModel();
+
+% Hybrid propagator
+simulation.propagators = {translationalPropagator, massPropagator};
+
+% Integrator
+simulation.integrator.type = Integrators.rungeKutta4;
+simulation.integrator.stepSize = 30;
+
+% Generate input file
+test.createInput(simulation,fullfile(mfilename,'main'));
+
+% Run test
+failcount = test.runUnitTest(mfilename);
+
