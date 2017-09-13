eb9e7cf4753ae9ad8f660d164597bee0e9953d20
Jacco Geul <jacco@geul.net>
2017-09-13 15:42:38 +0200

    Several build.m improvement/fixes for Linux.

--- build.m
+++ build.m
@@ -15,8 +15,22 @@ tudatTarget = 'tudat';
 testsTargetsPrefix = 'test_json_';
 
 cmakebin = '';
+% Default location on make if not installed to path
 if ismac
     cmakebin = '/Applications/CMake.app/Contents/bin/cmake';
+end
+if isunix || ismac
+    % Try to get cmake from path (UNIX)
+    [status, response] = system('which cmake');
+    if status == 0
+        cmakebin = strtrim(response);
+    end
+elseif ispc
+    % Try for Windows
+    [status, response] = system('where cmake');
+    if status == 0
+        cmakebin = strtrim(response);
+    end
 end
 if exist(cmakebin,'file') ~= 2
     cmakebin = input('Specify the absolute path to the cmake binary: ','s');
@@ -28,8 +42,8 @@ end
 
 command = [
     sprintf('cd %s; ',builddir)...
-    sprintf('%s ../tudatBundle; ',cmakebin)...
-    sprintf('%s --build . --target %s -- -j%i',cmakebin,tudatTarget,concurrentJobs)
+    sprintf('LD_LIBRARY_PATH= %s ../tudatBundle; ',cmakebin)...
+    sprintf('LD_LIBRARY_PATH= %s --build . --target %s -- -j%i',cmakebin,tudatTarget,concurrentJobs)
     ];
 
 if buildUnitTests
@@ -37,7 +51,7 @@ if buildUnitTests
     testNames = {testFiles.name};
     for i = 1:length(testNames)
         testName = strrep(testNames{i},'.m','');
-        command = sprintf('%s; %s --build . --target %s%s -- -j%i',...
+        command = sprintf('%s;LD_LIBRARY_PATH= %s --build . --target %s%s -- -j%i',...
             command,cmakebin,testsTargetsPrefix,testName,concurrentJobs);
     end
 end
