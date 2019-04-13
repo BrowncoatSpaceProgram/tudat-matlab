4da1b53c8c38914f92ac93157bfeda548970a0bc
BrowncoatSpaceProgram <42820930+BrowncoatSpaceProgram@users.noreply.github.com>
2019-04-12 21:23:31 -0400

    Update singlePerturbedSatellite.m

--- examples/seamless/singlePerturbedSatellite.m
+++ examples/seamless/singlePerturbedSatellite.m
@@ -1,4 +1,4 @@
-%% Perturbed Earth-orbiting Satellite
+%% Perturbed Earth-orbiting Satellite altered for Project 4
 
 clc; clear;
 tudat.load();
@@ -11,27 +11,18 @@ simulation.initialEpoch = 0;
 simulation.finalEpoch = convert.toSI(1,'d');
 simulation.globalFrameOrientation = 'J2000';
 
-% Bodies
+% Bodies - Altered with correct initial element values
 asterix = Body('asterix');
-asterix.initialState.semiMajorAxis = 7500e3;
-asterix.initialState.eccentricity = 0.1;
-asterix.initialState.inclination = deg2rad(85.3);
-asterix.initialState.argumentOfPeriapsis = deg2rad(235.7);
-asterix.initialState.longitudeOfAscendingNode = deg2rad(23.4);
-asterix.initialState.trueAnomaly = deg2rad(139.87);
-asterix.mass = 400;
-asterix.referenceArea = 4;
-asterix.dragCoefficient = 1.2;
-asterix.radiationPressureCoefficient = 1.2;
-asterix.radiationPressure.Sun.occultingBodies = {Earth};
-simulation.addBodies(Sun,Earth,Moon,Mars,Venus,asterix);
+asterix.initialState.semiMajorAxis = 7686.489e3;
+asterix.initialState.eccentricity = 0.1175;
+asterix.initialState.inclination = deg2rad(2.2);
+asterix.initialState.argumentOfPeriapsis = deg2rad(129.1);
+asterix.initialState.longitudeOfAscendingNode = deg2rad(230.8);
+asterix.initialState.trueAnomaly = deg2rad(0.91229);
+simulation.addBodies(Earth,asterix);
 
-% Accelerations
-accelerationsOnAsterix.Earth = {SphericalHarmonicGravity(5,5), AerodynamicAcceleration()};
-accelerationsOnAsterix.Sun = {PointMassGravity(), RadiationPressureAcceleration()};
-accelerationsOnAsterix.Moon = {PointMassGravity()};
-accelerationsOnAsterix.Mars = {PointMassGravity()};
-accelerationsOnAsterix.Venus = {PointMassGravity()};
+% Accelerations - extra bodies and accelerations removed, leaving non-uniform gravity force
+accelerationsOnAsterix.Earth = {SphericalHarmonicGravity(5,5)};
 
 % Propagator
 propagator = TranslationalPropagator();
@@ -44,37 +35,19 @@ simulation.propagators = {propagator};
 simulation.integrator.type = Integrators.rungeKutta4;
 simulation.integrator.stepSize = 10;
 
-% Variables to save
-% simulation.results.t and simulation.results.h will be set after calling simulation.run()
-simulation.addResultsToSave('t','independent');  % independent variable (time in seconds since J2000)
-simulation.addResultsToSave('h','asterix.altitude-Earth');  % altitude of Asterix wrt Earth
-
-
 %% RUN
 
 simulation.run();
+% simulation.fullSettings
 
 
 %% RESULTS
 
-% Plot altitude
-figure;
-plot(convert.epochToDate(simulation.results.t),simulation.results.h/1e3);
+% Plot distance to Earth's CoM - Altered to simply compare initial and final position and velocity vectors
+t = simulation.results.numericalSolution(:,1);
+r = simulation.results.numericalSolution(:,2:4);
+plot(convert.epochToDate(t),r/1e3);
 grid on;
-ylabel('Altitude [km]');
-
-
-%% RE-RUN SIMULATION FOR A SOLAR SAIL
-
-asterix.mass = 4;
-asterix.referenceArea = 40;
-simulation.run();
-
-
-%% ADD TO PLOT
-
-hold on;
-plot(convert.epochToDate(simulation.results.t),simulation.results.h/1e3);
-hold off;
-legend('Satellite','Solar sail','Location','South','Orientation','Horizontal');
+ylabel('Distance [km]');
+legend('x','y','z','Location','South','Orientation','Horizontal');
 
